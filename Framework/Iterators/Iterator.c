835476db5448b0faac89847e0e5bee8eef6c30a4
ann320 <ann320@14a3a9bf-bf50-4b75-a53e-d560239d81c6>
2014-10-16 15:38:55 +0000

    Updated iterators. A few more functions still need to be added before they can replace the simple iteration index of the task controllers.

--- Framework/Iterators/Iterator.c
+++ Framework/Iterators/Iterator.c
@@ -25,6 +25,9 @@
 struct Iterator {
 	char*					name;					// Iterator name.  
 	IterTypes				iterType;				// Iteratable objects type.
+	size_t					currentIterIdx;			// 0-based iteration index used to iterate over iterObjects
+	size_t					totalIter;				// Total number of iterations
+	DiscardDataFptr_type 	discardDataFptr;		// For data type iterators only, function callback to discard data from an iterator.
 	ListType				iterObjects;			// List of iteratable data objects such as Waveform_type* or Iterator_type* elements specified by iterType.
 	Iterator_type*			parent;					// Pointer to parent iterator, otherwise  NULL if root iterator.
 };
@@ -48,8 +51,11 @@ Iterator_type* init_Iterator_type (char name[])
 	if (!iterator) return NULL;
 	
 	iterator->name 					= StrDup(name);
+	iterator->currentIterIdx		= 0;
+	iterator->totalIter				= 0;
 	iterator->iterType				= Iterator_None;
 	iterator->iterObjects   		= 0;
+	iterator->discardDataFptr		= NULL;
 	iterator->parent				= NULL;
 	
 	if ( !(iterator->iterObjects   	= ListCreate(sizeof(void*))) )	goto Error;
@@ -60,5 +66,172 @@ Error:
 	if (iterator->iterObjects) ListDispose(iterator->iterObjects);
 	free(iterator);
 	return NULL;
+}
+
+void discard_Iterator_type (Iterator_type** iterator)
+{
+	if (!*iterator) return;
+	
+	OKfree((*iterator)->name);
+	
+	size_t 				nObjects 		= ListNumItems((*iterator)->iterObjects);
+	Iterator_type**		iterObjectPtr;
+	
+	for (size_t i = 1; i <= nObjects; i++) {
+		iterObjectPtr = ListGetPtrToItem((*iterator)->iterObjects, i);
+		if ((*iterator)->iterType == Iterator_Iterator)
+			discard_Iterator_type(iterObjectPtr);   							// discard recursively if object is another iterator
+		else
+			(*(*iterObjectPtr)->discardDataFptr)	((void**)iterObjectPtr);	// discard data if data type iterator
+	}
+	
+	ListDispose((*iterator)->iterObjects);
+	
+	// remove from parent iterator if any
+	if ((*iterator)->parent) {
+		nObjects = ListNumItems((*iterator)->parent->iterObjects);
+		for (size_t i = 1; i <= nObjects; i++) {
+			iterObjectPtr = ListGetPtrToItem((*iterator)->parent->iterObjects, i);
+			if (*iterObjectPtr == *iterator) {
+				ListRemoveItem((*iterator)->parent->iterObjects, 0, i);
+				break;
+			}
+			
+		}
+	}
+	
+	OKfree(*iterator);
+}
+
+IterTypes GetIteratorType (Iterator_type* iterator)
+{
+	return iterator->iterType;
+}
+
+size_t GetIteratorSize (Iterator_type* iterator)
+{
+	return ListNumItems(iterator->iterObjects);
+}
+
+void SetIteratorIterations (Iterator_type* iterator, size_t nIterations)
+{
+	iterator->totalIter = nIterations;
+}
+
+void GetCurrentIterationIndices	(Iterator_type* iterator, size_t currentIterIndex[])
+{
+	Iterator_type**		iterObjectPtr	= &iterator;
+	size_t				idx				= 0;
+	
+	currentIterIndex[idx] = (*iterObjectPtr)->currentIterIdx; 
+	
+	while ((*iterObjectPtr)->iterType == Iterator_Iterator) {
+		// get next iterator
+		iterObjectPtr = ListGetPtrToItem((*iterObjectPtr)->iterObjects, currentIterIndex[idx]+1); 
+		idx++;
+		// get current iteration index of this iterator
+		currentIterIndex[idx] = (*iterObjectPtr)->currentIterIdx;
+	}
+}
+
+size_t GetIteratorDimensions (Iterator_type* iterator)
+{
+	Iterator_type**		iterObject	= &iterator;
+	size_t				nDim;
+		
+	if (ListNumItems(iterator->iterObjects))
+		nDim = 1;
+	else
+		nDim = 0;
+	
+	while ((*iterObject)->iterType == Iterator_Iterator && ListNumItems((*iterObject)->iterObjects)) {
+		iterObject = ListGetPtrToItem((*iterObject)->iterObjects, 1);
+		nDim++;
+	}
+	
+	return nDim;
+}
+
+Iterator_type* GetRootIterator (Iterator_type* iterator)
+{
+	Iterator_type* rootIterator		= iterator;
+	
+	while (rootIterator->parent)
+		rootIterator = iterator->parent;
+	
+	return rootIterator; 
+}
+
+BOOL IsRootIterator	(Iterator_type* iterator)
+{
+	if (iterator->parent) 
+		return TRUE;
+	else
+		return FALSE;
+}
+
+int	IteratorAddWaveform (Iterator_type* iterator, Waveform_type* waveform)
+{
+#define IteratorAddWaveform_Err_WrongIteratorType		-1
+	
+	// Adding a waveform to an iterator determines its type. Once an iterator is of waveform type
+	// only waveform can be added to it.
+	
+	if (iterator->iterType == Iterator_None) {
+		iterator->iterType 			= Iterator_Waveform;
+		iterator->discardDataFptr 	= (DiscardDataFptr_type) discard_Waveform_type; 
+	}
+	else
+		if (iterator->iterType != Iterator_Waveform) return IteratorAddWaveform_Err_WrongIteratorType;
+	
+	ListInsertItem(iterator->iterObjects, &waveform, END_OF_LIST);
+	
+	return 0; // success
+}
+
+int	IteratorAddIterator	(Iterator_type* iterator, Iterator_type* iteratorToAdd)
+{
+#define IteratorAddIterator_Err_WrongIteratorType					-1
+	// iterator to be added as a child has been already added to another iterator
+#define IteratorAddIterator_Err_ChildIteratorAlreadyAssigned		-2
+	
+	// check parent iterator type
+	if (iterator->iterType == Iterator_None)  
+		iterator->iterType 			= Iterator_Iterator;
+	else
+		if (iterator->iterType != Iterator_Iterator) return IteratorAddIterator_Err_WrongIteratorType;
+	
+	// check if iteratorToAdd has already a parent iterator
+	if (iteratorToAdd->parent) return IteratorAddIterator_Err_ChildIteratorAlreadyAssigned;
+	
+	// add child iterator to parent iterator
+	ListInsertItem(iterator->iterObjects, &iteratorToAdd, END_OF_LIST);
+	
+	// add parent iterator to child iterator
+	iteratorToAdd->parent = iterator;
+	
+	return 0;
+}
+
+void ResetAllIterators (Iterator_type* iterator)
+{
+	size_t 				nObjects 		= ListNumItems(iterator->iterObjects);
+	Iterator_type**		iterObjectPtr;
+	
+	// reset iteration indices recursively for iterators of iterators	
+	if (iterator->iterType == Iterator_Iterator) 
+		for (size_t i = 1; i <= nObjects; i++) {
+			iterObjectPtr = ListGetPtrToItem(iterator->iterObjects, i);
+			ResetAllIterators(*iterObjectPtr);   				
+		}
+	
+	// reset current iteration index of this iterator (regardless of iterator type)
+	iterator->currentIterIdx = 0;
+}
+
+BOOL AdvanceIterationIndex (Iterator_type* iterator, size_t currentIterIndex[])
+{
+	
+	
 }
 
--- Framework/Iterators/Iterator.h
+++ Framework/Iterators/Iterator.h
@@ -32,11 +32,13 @@ typedef struct Iterator		Iterator_type;
 
 typedef enum {
 	
-	Iterator_None,					// Default for newly created iterators
-	Iterator_Iterator,			  	// Iterator over other Iterator_type* iterators.
-	Iterator_Waveform			  	// Iterator over Waveform_type* waveforms.
+	Iterator_None,												// Default for newly created iterators
+	Iterator_Iterator,			  								// Iterator over other Iterator_type* iterators.
+	Iterator_Waveform			  								// Iterator over Waveform_type* waveforms.
 	
 } IterTypes;
+
+typedef void (*DiscardDataFptr_type) (void** PtrToData);		// Function pointer for discarding data contained in data type iterators.
 
 //==============================================================================
 // External variables
@@ -48,30 +50,62 @@ typedef enum {
 // Iterator Creation / Destruction functions
 //---------------------------------------------------------------------------------------------------------------------------
 
-	// Creates iterator
+	// Creates iterator.
 Iterator_type*			init_Iterator_type			(char name[]);
 
 	// Discards a given iterator and all the data or iterators contained within recursively
 void					discard_Iterator_type		(Iterator_type** iterator);
 
 //---------------------------------------------------------------------------------------------------------------------------
-// Get functions
+// Set/Get functions
 //---------------------------------------------------------------------------------------------------------------------------
 
+	// Returns the type of an iterator
 IterTypes				GetIteratorType				(Iterator_type* iterator);
 
+	// Returns the number of iteratable objects 
 size_t					GetIteratorSize				(Iterator_type* iterator);
 
+	// Set total number of iterations for a given iterator
+void					SetIteratorIterations		(Iterator_type* iterator, size_t nIterations);
+
+	// Returns the current 0-based iteration indices of the iterator set. Provide a currentIterIndex buffer with at
+	// least as many elements as obtained from GetIteratorDimensions. Note:To return all indices apply this
+	// function to the root iterator.
+void					GetCurrentIterationIndices	(Iterator_type* iterator, size_t currentIterIndex[]);
+
+	// Returns the dimensionality of the iterator. Note: The dimensionality is calculated by considering its children iterators.
+	// To get the dimensionality of the entire iterator set, apply this function to the root iterator
 size_t					GetIteratorDimensions		(Iterator_type* iterator);
 
+	// Returns the root iterator of an iterator set. If this is already a root iterator, it returns the same pointer.
+Iterator_type*			GetRootIterator				(Iterator_type* iterator);
+
+	// Returns TRUE if the provided iterator is a root iterator, i.e. it does not have another parent iterator, and FALSE otherwise
+BOOL					IsRootIterator				(Iterator_type* iterator);
 
 //---------------------------------------------------------------------------------------------------------------------------
 // Iterator composition
 //---------------------------------------------------------------------------------------------------------------------------
 
+	// Adds a waveform to an iterator
 int						IteratorAddWaveform			(Iterator_type* iterator, Waveform_type* waveform);
 
+	// Adds an iterator to another iterator
 int						IteratorAddIterator			(Iterator_type* iterator, Iterator_type* iteratorToAdd);
+
+//---------------------------------------------------------------------------------------------------------------------------
+// Iterator iteration control
+//---------------------------------------------------------------------------------------------------------------------------
+
+	// Resets the current 0-based iteration index for the given iterator and all its child iterators recursively to 0.
+	// Note: To reset iterations for all the iterators in the interation set, apply this function to the root iterator
+void					ResetAllIterators			(Iterator_type* iterator);
+
+	// Advances the iteration of a composite iterator. Valid only for root iterators. Provide currentIterIndex array buffer with at least
+	// as many elements as the iterator dimension obtained by calling GetIteratorDimensions. Returns TRUE if the iterator can be advanced
+	// further, and FALSE if there are no more iterations.
+BOOL					AdvanceIterationIndex		(Iterator_type* iterator, size_t currentIterIndex[]);
 
 
 

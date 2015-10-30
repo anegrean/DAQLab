c67cfc5e8f86d0742f0fe2635f443ef8598c8f3a
anegrean <negreanadrian@gmail.com>
2015-10-30 18:23:16 +0100

    Generic image display
    Cleaned up the generic image display. Just msg me for questions.

--- Framework/Display/ImageDisplay.c
+++ Framework/Display/ImageDisplay.c
@@ -1,16 +1,17 @@
-/*GenericImageDisplay_type  <- clasa parinte = interfata ca sa cadem de acord
-
-ImageDisplayCVI_type*/
-
-// look in displayEngine .h and .c 
-
+//==============================================================================
+//
+// Title:		ImageDisplay.c
+// Purpose:		Generic image display interface.
+//
+// Created on:	16-7-2015 at 13:16:19 by Adrian Negrean.
+// Copyright:	VU University Amsterdam. All Rights Reserved.
+//
 
 #include "ImageDisplay.h"
+#include "DAQLabUtility.h"
 
 //==============================================================================
 // Constants
-
-#define OKfree(ptr) if (ptr) {free(ptr); ptr = NULL;}
 
 //==============================================================================
 // Types
@@ -27,22 +28,65 @@ ImageDisplayCVI_type*/
 //==============================================================================
 // Global functions
 
-void init_GenericImageDisplay_type (GenericImageDisplay_type* 		ImageDisplay,
-									DiscardFptr_type				imageDiscardFptr,
-									DisplayImageFptr_type			displayImageFptr,
-								 	CallbackGroup_type**			callbackGroupPtr)
+int init_ImageDisplay_type (ImageDisplay_type* 				imageDisplay,
+							DiscardFptr_type				imageDisplayDiscardFptr,
+							DisplayImageFptr_type			displayImageFptr,
+							CallbackGroup_type**			callbackGroupPtr)
 {
+INIT_ERR
 	
-	// DATA
+	Image_type**	imagePtr	= NULL;
 	
-	// METHODS
+	//----------------------------------------------------------
+	// Init
+	//----------------------------------------------------------
 	
-	ImageDisplay->imageDiscardFptr 			= imageDiscardFptr;
-	ImageDisplay->displayImageFptr			= displayImageFptr;
+	// data
+	imageDisplay->imageTSV					= 0;
 	
-	// CALLBACKS
+	// methods
+	imageDisplay->imageDisplayDiscardFptr 	= imageDisplayDiscardFptr;
+	imageDisplay->displayImageFptr			= displayImageFptr;
 	
-	ImageDisplay->callbackGroup				= *callbackGroupPtr;
+	// callbacks
+	imageDisplay->callbackGroup				= *callbackGroupPtr;
 	*callbackGroupPtr						= NULL;
 	
+	//----------------------------------------------------------
+	// Alloc
+	//----------------------------------------------------------
+	
+	errChk( CmtNewTSV(sizeof(Image_type*), &imageDisplay->imageTSV) );
+	// init image TSV
+	errChk( CmtGetTSVPtr(imageDisplay->imageTSV, &imagePtr) );
+	*imagePtr = NULL;
+	CmtReleaseTSVPtr(imageDisplay->imageTSV);
+	
+Error:
+	
+	return errorInfo.error;
+}
+
+void discard_ImageDisplay_type (ImageDisplay_type** imageDisplayPtr)
+{
+	ImageDisplay_type* 		imageDisplay 	= *imageDisplayPtr;
+	Image_type**			imagePtr		= NULL;
+	
+	if (!imageDisplay) return;
+	
+	// dispose image
+	if ( CmtGetTSVPtr(imageDisplay->imageTSV, &imagePtr)  < 0) goto SkipDiscardImage;
+	discard_Image_type(imagePtr);
+	CmtReleaseTSVPtr(imageDisplay->imageTSV);
+	
+SkipDiscardImage:
+	// discard image TSV
+	if (imageDisplay->imageTSV) {
+		CmtDiscardTSV(imageDisplay->imageTSV);
+		imageDisplay->imageTSV = 0;
+	}
+	
+	discard_CallbackGroup_type(&imageDisplay->callbackGroup);
+	
+	OKfree(*imageDisplayPtr);
 }
--- Framework/Display/ImageDisplay.h
+++ Framework/Display/ImageDisplay.h
@@ -1,7 +1,7 @@
 //==============================================================================
 //
 // Title:		ImageDisplay.h
-// Purpose:		A short description of the interface.
+// Purpose:		Generic image display interface.
 //
 // Created on:	16-7-2015 at 13:16:19 by Adrian Negrean.
 // Copyright:	VU University Amsterdam. All Rights Reserved.
@@ -21,87 +21,124 @@
 #include "cvidef.h"
 #include "DataTypes.h"
 
-//==============================================================================
-// Constants
 
 //==============================================================================
 // Types
-	//------------
-	// Classes
-	//------------
-	
-	typedef struct genericImageDisplay		GenericImageDisplay_type;	  // Generic image display class
-	
-	//------------													
-	// Data types
-	//------------
-	typedef void* GetImgDisplayCBData_type;
-	typedef void* ImgDisplayCBData_type; 
-	
-	// TO DO:
-	
-	// add enum with minimum set of events
-	// add extra event parameter to image display CB
-	
-	//--------------------------------------
-	// Callback typedefs
-	//--------------------------------------
-		// Displays or updates an image in a display window
-	typedef int								(*DisplayImageFptr_type)					(GenericImageDisplay_type* imgDisplay, Image_type** image);
 
-	// Generic function type to dispose of dinamically allocated data
-	typedef void 							(*DiscardFptr_type) 						(void** objectPtr);    
-		// Obtains a display handle from the display engine that can be passed to other functions like updating the image
-	typedef GenericImageDisplay_type*		(*GetImageDisplayFptr_type)					(GenericImageDisplay_type* displayEngine, void* callbackData, int imgHeight, int imgWidth, ImageTypes imageType);
 
-		// Returns the callback data associated with the display handle
-	typedef GetImgDisplayCBData_type		(*GetImageDisplayCBDataFptr_type)			(GenericImageDisplay_type* imgDisplay);
+typedef struct ImageDisplay						ImageDisplay_type;					// Generic image display class to inherit from.
 
+typedef struct ChannelGroupDisplay				ChannelGroupDisplay_type;			// Used to group several image displays that show an image in different color channels or combined in a composite image. 
+																					// All images must have the same dimension and they share the same regions of interest (ROIs).
+																					
+typedef struct ChannelGroupDisplayContainer		ChannelGroupDisplayContainer_type;	// Container for multiple channel display groups. Image dimensions between different channel groups need not be the same.
+																	
+
+//--------------------------------------------------------------
+// Functions
+//--------------------------------------------------------------
+
+// Displays or updates an image in a display window
+typedef int				(*DisplayImageFptr_type)					(ImageDisplay_type* imgDisplay, Image_type** image);
+
+//--------------------------------------------------------------		
+// Enums
+//--------------------------------------------------------------		
+
+// Image display events
+typedef enum {
+	// General
+	ImageDisplay_Discard,											// Image display window was discarded.
+	ImageDisplay_RestoreSettings,									// The image display window wishes to restore a list of settings that were used to acquire the image.
 	
-//==============================================================================
-// Classes to inherit from
+	// Region of interest (ROI) management
+	ImageDisplay_ROI_Placed,										// A ROI (region of interest) was placed on the image, but not added to it.
+	ImageDisplay_ROI_Added,											// A ROI was added to the image.
+	ImageDisplay_ROI_Removed,										// A ROI was removed from the image.
 	
-	struct genericImageDisplay {  //parent obj
-	//---------------------------------------------------------------------------------------------------------------
+} ImageDisplayEvents;
+
+// Allowed operations on ROIs
+typedef enum {
+	ROI_Visible,													// ROI is visible on the image.
+	ROI_Hide,														// ROI is hidden.
+	ROI_Delete														// ROI is deleted from the image.
+} ROIActions;
+
+//--------------------------------------------------------------		
+// Generic image display class
+//--------------------------------------------------------------
+		
+struct ImageDisplay {
+	//----------------------------------------------------
 	// DATA
-	//---------------------------------------------------------------------------------------------------------------
+	//----------------------------------------------------
+	
+	CmtTSVHandle						imageTSV;					// Thread safe variable of Image_type* storing the displayed image which can be accessed safely from multiple threads.
 	
 	
-	
-	//---------------------------------------------------------------------------------------------------------------
+	//----------------------------------------------------
 	// METHODS (override with child class implementation)
-	//---------------------------------------------------------------------------------------------------------------
+	//----------------------------------------------------
 	
-	DiscardFptr_type					imageDiscardFptr;				// Method to discard imageData.
-	
-	DisplayImageFptr_type				displayImageFptr;
-	
-	GetImageDisplayFptr_type			getImageFptr;
-
-	//---------------------------------------------------------------------------------------------------------------
-	// CALLBACKS (provide action to be taken by the module making use of the display engine)
-	//---------------------------------------------------------------------------------------------------------------
-	
-	
-	CallbackGroup_type*					callbackGroup; 
-	
-	}; 
+	DiscardFptr_type					imageDisplayDiscardFptr;	// Method to discard the image display.
+	DisplayImageFptr_type				displayImageFptr;			// Method to display an image.
 	
 
+	//----------------------------------------------------
+	// CALLBACKS
+	//----------------------------------------------------
 	
-//==============================================================================
-// External variables
-
+	CallbackGroup_type*					callbackGroup;				// Callback function of the form CallbackFptr_type within the callback group receive events of ImageDisplayEvents type.  			   
+};
+	
+	
 //==============================================================================
 // Global functions
 
-	
-// Initializes a generic display engine instance.
-	
-void									init_GenericImageDisplay_type			(GenericImageDisplay_type* 				ImageDisplay,
-																				 DiscardFptr_type						imageDiscardFptr,
-																				 DisplayImageFptr_type					displayImageFptr,
-																				 CallbackGroup_type**					callbackGroupPtr);
+
+//--------------------------------------------------------------------------------------------------------------------------
+// Generic image display class management
+//--------------------------------------------------------------------------------------------------------------------------
+
+// Initializes the generic image display class.
+int										init_ImageDisplay_type						(ImageDisplay_type* 		imageDisplay,
+																			 	 	 DiscardFptr_type			imageDisplayDiscardFptr,
+																				  	 DisplayImageFptr_type		displayImageFptr,
+																			 	 	 CallbackGroup_type**		callbackGroupPtr);
+
+void									discard_ImageDisplay_type					(ImageDisplay_type** imageDisplayPtr);
+
+//--------------------------------------------------------------------------------------------------------------------------
+// Channel Group Display
+//--------------------------------------------------------------------------------------------------------------------------
+
+// Creates a channel display group with index chanDisplayGroupIdx containing up to nChannels.
+ChannelGroupDisplay_type*				init_ChannelGroupDisplay_type				(char name[]);
+
+void									discard_ChannelGroupDisplay_type			(ChannelGroupDisplay_type** chanGroupDisplayPtr);
+
+// Given a 1-based ROI index, the function performs an action on the ROI from a given channel display within the group. If channelIdx is 0 then the action applies to all channels.
+// If ROIIdx is 0 then the action applies to all ROIs.
+int										ChannelGroupDisplayROIAction				(ChannelGroupDisplay_type* chanGroupDisplay, size_t channelIdx, size_t ROIIdx, ROIActions action, char** errorMsg);
+
+// Adds an image to the channel group display with a given channel index. Any existing ROIs from the provided image are discarded and they must be added back afterwards. The image width and height in pixels
+// must be the same for all images added to the channel group display.
+int										ChannelGroupDisplayAddImageChannel			(ChannelGroupDisplay_type* chanGroupDisplay, size_t chanIdx, Image_type** imagePtr);
+
+//--------------------------------------------------------------------------------------------------------------------------
+// Channel Group Display Container
+//--------------------------------------------------------------------------------------------------------------------------
+
+ChannelGroupDisplayContainer_type*		init_ChannelGroupDisplayContainer_type		(void);
+
+void									discard_ChannelGroupDisplayContainer_type	(ChannelGroupDisplayContainer_type** containerPtr);
+
+// Adds a channel group display to the container and returns its 1-based handle index within the container. On failure the function returns 0.
+size_t									AddChannelGroupDisplayToContainer			(ChannelGroupDisplayContainer_type* container, ChannelGroupDisplay_type** chanGroupDisplayPtr);
+
+// Gets the 1-based active channel group index within the container. If 0, there is no active channel group display.
+size_t									GetActiveChannelGroupDisplayIndex			(ChannelGroupDisplayContainer_type* container);
  
 
 #ifdef __cplusplus
@@ -110,13 +147,6 @@ void									init_GenericImageDisplay_type			(GenericImageDisplay_type* 				Imag
 
 #endif  /* ndef __ImageDisplay_H__ */
 
- /*
-GenericImageDisplay_type  <- clasa parinte = interfata ca sa cadem de acord
 
-ImageDisplayCVI_type
-
-// look in displayEngine .h and .c 
-
-*/
 
 
